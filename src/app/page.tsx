cde70247af6bc13b44eb96ad62bf80d3017e1165
Unsociable <unsociablestreamer@googlemail.com>
2023-08-04 20:14:56 +0100

    Made responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,79 +4,86 @@ import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
-    <div className="flex flex-col p-24 text-off-white bg-gradient-to-b from-bg-img-top to-bg-img-btm">
+    <div className="flex flex-col text-off-white bg-gradient-to-b from-bg-img-top to-bg-img-btm">
       <main
-        className="flex min-h-screen flex-col items-center justify-between relative"
+        className="flex min-h-screen flex-col items-center justify-between"
         id="home"
       >
         <Navbar />
-        <img
-              src="/pictures/background/header-bg.png"
-              width="800"
-              height="800"
-              alt="A laptop in a cyber punk style"
-              className="absolute blur-sm"
-            />
-        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 space-y-4 py-6 drop-shadow-2xl">
-          <h1 className="text-center font-poppins text-7xl font-extrabold">
-            Andrew Cade
-          </h1>
-          <h2 className="text-center font-poppins text-4xl">
-            Software Engineer & Developer
-          </h2>
+        <div className="sm:relative">
+          <img
+            src="/pictures/background/header-bg.png"
+            width="800"
+            height="800"
+            alt="A laptop in a cyber punk style"
+            className="sm:w-full sm:h-full object-cover blur-sm invisible sm:visible"
+          />
+          <div className="absolute inset-0 flex flex-col items-center justify-center space-y-4 py-6">
+            <h1 className="text-center font-poppins sm:text-7xl text-xl font-extrabold text-white">
+              Andrew Cade
+            </h1>
+            <h2 className="text-center font-poppins sm:text-4xl text-lg text-white pt-2 sm:pt-0">
+              Software Engineer & Developer
+            </h2>
+          </div>
         </div>
       </main>
-      <section
-        className="flex flex-col items-center min-h-screen pt-20"
-        id="knowledge"
-      >
-        <h1 className="text-center text-6xl font-poppins font-bold">
+      <section className="flex flex-col items-center min-h-screen pt-20" id="knowledge">
+        <h1 className="text-center sm:text-xl lg:text-6xl font-poppins font-bold">
           My Knowledge
         </h1>
-        <div className="flex justify-between px-4 py-8 my-20 space-x-6 border">
-          <div className="flex flex-col items-start space-y-4">
-            <div className="flex items-start space-x-10">
-            <img
-              src="/pictures/icons/monitor.png"
-              width="70"
-              height="70"
-              alt="A monitor outline"
-              className="p-2"
-            />
-              <h2 className="text-5xl font-poppins font-bold">Software<br/>Development</h2>
+
+        <div className="w-full sm:w-3/4 p-5 md:grid md:grid-cols-3 gap-5 mt-10 sm:mt-20 font-poppins text-off-white sm:border">
+          <div className="flex flex-wrap justify-center items-center">
+            <div className="flex flex-row flex-wrap items-center">
+              <img
+                src="/pictures/icons/monitor.png"
+                width="70"
+                height="70"
+                alt="A monitor outline"
+                className="shrink-0"
+              />
+              <h2 className="pl-5 self-center sm:text-m lg:text-4xl font-bold">Software<br />Development</h2>
             </div>
-            <p className="px-10 py-5">This is a description for image 1.</p>
+            <p className="mt-3 sm:mt-0 sm:p-5 sm:text-left text-sm">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vitae proin sagittis nisl rhoncus mattis rhoncus urna. Sapien faucibus et molestie ac feugiat sed. Sed turpis tincidunt id aliquet risus. Et ultrices neque ornare aenean euismod elementum. Sed felis eget velit aliquet sagittis id consectetur purus ut. Egestas congue quisque egestas diam. Odio tempor orci dapibus ultrices in iaculis nunc sed augue. Nibh mauris cursus mattis molestie a iaculis at erat. Arcu non odio euismod lacinia. Non blandit massa enim nec. Vitae ultricies leo integer malesuada nunc vel risus commodo. Facilisis sed odio morbi quis commodo odio aenean. Volutpat blandit aliquam etiam erat.
+            </p>
           </div>
 
-          <div className="flex flex-col items-start space-y-4">
-            <div className="flex items-start space-x-4">
-            <img
-              src="/pictures/icons/design.png"
-              width="70"
-              height="70"
-              alt="A design outline"
-              className="p-2"
-            />
-              <h2>Title 2</h2>
+          <div className="flex flex-wrap justify-center items-center">
+            <div className="flex flex-row flex-wrap items-center">
+              <img
+                src="/pictures/icons/monitor.png"
+                width="70"
+                height="70"
+                alt="A monitor outline"
+                className="shrink-0"
+              />
+              <h2 className="pl-5 self-center sm:text-m lg:text-4xl font-bold">Software<br />Development</h2>
             </div>
-            <p>This is a description for image 2.</p>
+            <p className="mt-3 sm:mt-0 sm:p-5 sm:text-left text-sm">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vitae proin sagittis nisl rhoncus mattis rhoncus urna. Sapien faucibus et molestie ac feugiat sed. Sed turpis tincidunt id aliquet risus. Et ultrices neque ornare aenean euismod elementum. Sed felis eget velit aliquet sagittis id consectetur purus ut. Egestas congue quisque egestas diam. Odio tempor orci dapibus ultrices in iaculis nunc sed augue. Nibh mauris cursus mattis molestie a iaculis at erat. Arcu non odio euismod lacinia. Non blandit massa enim nec. Vitae ultricies leo integer malesuada nunc vel risus commodo. Facilisis sed odio morbi quis commodo odio aenean. Volutpat blandit aliquam etiam erat.
+            </p>
           </div>
 
-          <div className="flex flex-col items-start space-y-4">
-            <div className="flex items-start space-x-4">
-            <img
-              src="/pictures/icons/design.png"
-              width="70"
-              height="70"
-              alt="A design outline"
-              className="p-2"
-            />
-              <h2>Title 3</h2>
+          <div className="flex flex-wrap justify-center items-center">
+            <div className="flex flex-row flex-wrap items-center">
+              <img
+                src="/pictures/icons/monitor.png"
+                width="70"
+                height="70"
+                alt="A monitor outline"
+                className="shrink-0"
+              />
+              <h2 className="pl-5 self-center sm:text-m lg:text-4xl font-bold">Software<br />Development</h2>
             </div>
-            <p>This is a description for image 3.</p>
+            <p className="mt-3 sm:mt-0 sm:p-5 sm:text-left text-sm">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vitae proin sagittis nisl rhoncus mattis rhoncus urna. Sapien faucibus et molestie ac feugiat sed. Sed turpis tincidunt id aliquet risus. Et ultrices neque ornare aenean euismod elementum. Sed felis eget velit aliquet sagittis id consectetur purus ut. Egestas congue quisque egestas diam. Odio tempor orci dapibus ultrices in iaculis nunc sed augue. Nibh mauris cursus mattis molestie a iaculis at erat. Arcu non odio euismod lacinia. Non blandit massa enim nec. Vitae ultricies leo integer malesuada nunc vel risus commodo. Facilisis sed odio morbi quis commodo odio aenean. Volutpat blandit aliquam etiam erat.
+            </p>
           </div>
         </div>
       </section>
+
       <section className="flex min-h-screen" id="work">
         {" "}
         {/* replace with your preferred classes */}
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -21,8 +21,8 @@ const Navbar = () => {
   const linkClass = isScrolled ? "text-picton-blue font-bold font-poppins" : "text-off-white font-bold font-poppins";
 
     return (
-        <nav className="w-3/4 bg-transparent fixed top-0 z-50 pt-5 pb-5 transition-all duration-500 ease-in-out transform backdrop-blur">
-            <div className='flex flex-row justify-around'>
+        <nav className="sm:w-3/4 bg-transparent fixed top-0 z-50 sm:py-5 py-10 transition-all duration-500 ease-in-out transform backdrop-blur w-full">
+            <div className='flex flex-col sm:flex-row justify-center sm:justify-around items-center gap-5'>
                 <a className={linkClass} href="#home">{'<home />'}</a>
                 <a className={linkClass} href="#knowledge">{'<knowledge />'}</a>
                 <a className={linkClass} href="#work">{'<work />'}</a>
